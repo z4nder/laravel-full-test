0e240b04164bf17058253b73909594496de9b1e8
Gustavo Silva <gustavo.silva@codificar.com.br>
2021-05-10 23:46:36 -0300

    Comment Controller Functions

--- app/Http/Controllers/DailyLogController.php
+++ app/Http/Controllers/DailyLogController.php
@@ -16,10 +16,17 @@ class DailyLogController extends Controller
       $this->middleware('user.block.name', ['only' => ['store']]);
     }
 
+   /**
+    * Method store user DailyLog
+    *
+    * @param DailyLogStoreRequest $request
+    *
+    * @return void
+   */
    public function store(DailyLogStoreRequest $request)
     {
       $input = $request->validated();
-    
+
       $dailyLog = $request->user()->dailyLogs()->create($input);
 
       event(new DailyLogCreated($dailyLog));
@@ -27,6 +34,13 @@ class DailyLogController extends Controller
       return back();
     }
 
+    /**
+     * Method update user DailyLog
+     *
+     * @param DailyLog $dailyLog
+     *
+     * @return void
+   */
     public function update(DailyLog $dailyLog)
     {
       $dailyLog->update(request()->only('log'));
@@ -34,12 +48,19 @@ class DailyLogController extends Controller
       return back();
     }
 
+    /**
+     * Method delete user DailyLog
+     *
+     * @param DailyLog $dailyLog
+     * @throws UserNotAuthorizedException
+     * @return void
+    */
     public function delete(DailyLog $dailyLog)
     {
-      if (! Gate::allows('delete-dailyLog', $dailyLog)) abort(403);
-      
+      if (!Gate::allows('delete-dailyLog', $dailyLog)) abort(403);
+
       $dailyLog->delete();
 
       return back();
-    }    
+    }
 }
